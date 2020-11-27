4b25568b1051c7ef7a7c44752a645b45ce3459c9
joshcode4real <71887588+joshcode4real@users.noreply.github.com>
2020-11-27 04:00:32 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import Weather from './Weather/Weather'
+// import Mvp from "../src/mvp.css"
+// import Card from './card/Card'
+// import MainNav from './Monday/MainNav'
+// import BodyAM from './Body_Component/Body.js'
+// import { ViewEntry } from './component/ViewEntry.js'
+// import Body from './Body_Component/Body'
+// import {BrowserRouter as Router,
+// Route,
+// Switch} from 'react-router-dom'
+// import Counter from './Monday/Counter/Counter'
+// import BMI from './Monday/BMI/BMI'
+// import Home_page from './movie_pacages/Home_page'
+// import Home_page from "./movie_pacages/Home_page";
+// import Movies from "./movie_pacages/Movies/Movies";
+// import About from "./movie_pacages/About/About";
+// import Contact from "./movie_pacages/Contact/Contact";
+// import Header from './Head/Header';
+// import Lagos from './Lagos/Lagos';
+
+
+function App() {
+  return (
+    // <Router>
+    //   <Switch>
+    //     <Route path = "/" exact component = {MainNav}/>
+    //     <Route path = "/Counter" exact component = {Counter}/>
+    //     <Route path = "/BMI" exact component = {BMI}/>
+    //   </Switch>
+    // </Router>
+    // <div>
+    //   <Card/>
+    // </div>
+    //  <Router>
+    //   <Switch>
+    //     <Route path = "/" exact component = {Home_page}/>
+    //     <Route path = "/Movies" exact component = {Movies}/>
+    //     <Route path = "/About" exact component = {About}/>
+    //     <Route path = "/Contact" exact component = {Contact}/>
+    //   </Switch>
+    // // </Router>
+    // <section>
+    //     {/* <Header/> */}
+        // <Lagos/>
+    // </section>
+    <Weather/>
+  )
+}
+
+export default App
